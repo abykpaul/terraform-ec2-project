57f8253e138ea2e236824f49d9b7e91056111f5f
abykpaul <44528042+abykpaul@users.noreply.github.com>
2025-06-25 08:55:42 +0530

    Create variables.tf

---
+++ jenkins_terraform_json_pipeline/variables.tf
@@ -0,0 +1,14 @@
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key pair name"
+  type        = string
+}
+
+variable "owner" {
+  description = "Owner name tag"
+  type        = string
+}
